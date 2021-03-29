012c80e35ae17bc81fc8b794cb1abead9f4ea70c
Tom <tom-vautray@hotmail.fr>
2021-03-29 21:21:25 +0200

    Added class ImageResource

---
+++ src/resourcePackage/ImageResource.java
@@ -0,0 +1,12 @@
+package resourcePackage;
+
+import java.io.File;
+import java.net.URL;
+
+public class ImageResource extends Resource
+{
+	public ImageResource(int localId, URL url, File file, String name)
+	{
+		super(localId, url, file, name);
+	}
+}
