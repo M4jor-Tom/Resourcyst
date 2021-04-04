c6df1a9129c025a325775f58cc60b3b855730dd8
Tom <tom-vautray@hotmail.fr>
2021-04-04 18:23:37 +0200

    Added attribute name to class Album

--- src/resourcePackage/Album.java
+++ src/resourcePackage/Album.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 public class Album
 {
 	private ArrayList<Author> _authors;
+	private String _name;
 	
-	public Album(Author author)
+	public Album(Author author, String name)
 	{
 		setAuthors(new ArrayList<>());
 		getAuthors().add(author);
+		setName(name);
 	}
 	
-	public Album(ArrayList<Author> authors)
+	public Album(ArrayList<Author> authors, String name)
 	{
 		setAuthors(new ArrayList<>());
 		setAuthors(authors);
+		setName(name);
 	}
 
 	public ArrayList<Author> getAuthors()
@@ -26,5 +29,15 @@ public class Album
 	public void setAuthors(ArrayList<Author> authors)
 	{
 		_authors = authors;
+	}
+
+	public String getName()
+	{
+		return _name;
+	}
+
+	public void setName(String name)
+	{
+		_name = name;
 	}
 }
