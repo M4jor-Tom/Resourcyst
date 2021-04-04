dfdc94031524b37f269ef939cf6215e9233c1a7b
Tom <tom-vautray@hotmail.fr>
2021-04-04 18:24:30 +0200

    Added class MusicResource constructor for multi-artists albums

--- src/resourcePackage/MusicResource.java
+++ src/resourcePackage/MusicResource.java
@@ -6,10 +6,21 @@ import java.net.URL;
 public class MusicResource extends AudioResource
 {
 	private Album _album;
+
+	public MusicResource(URL url, File file, String name, Album album)
+	{
+		super(url, file, name, null);
+		
+		//Case of a multi-artists Album
+		setAlbum(album);
+	}
 	
 	public MusicResource(URL url, File file, String name, Author author)
 	{
 		super(url, file, name, author);
+		
+		//Case of a single
+		setAlbum(new Album(author, name));
 	}
 
 	public Album getAlbum()
