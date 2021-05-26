f42d41a3b4be79d0ffec40f1c939482449c2ae1e
Tom <tom-vautray@hotmail.fr>
2021-05-26 21:55:51 +0200

    Added static ApplicationDatabase.getInstance(Context) method

--- src/resourcePackage/roomClasses/ApplicationDatabase.java
+++ src/resourcePackage/roomClasses/ApplicationDatabase.java
@@ -1,6 +1,9 @@
 package com.example.androidmusyst.Resourcyst.src.resourcePackage.roomClasses;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(
@@ -12,5 +15,21 @@ import androidx.room.RoomDatabase;
 )
 public abstract class ApplicationDatabase extends RoomDatabase
 {
+    private static volatile ApplicationDatabase _instance;
+
+    public static ApplicationDatabase getInstance(Context context)
+    {
+        if(_instance == null)
+            synchronized(ApplicationDatabase.class)
+            {
+                if (_instance == null)
+                    _instance = Room.databaseBuilder(context.getApplicationContext(),
+                            ApplicationDatabase.class, "resourcyst_db")
+                            .build();
+            }
+
+        return _instance;
+    }
+
     public abstract ResourceDao resourceDao();
 }
